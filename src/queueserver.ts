d77384ffb74a28c40f70f340c6f91fc9bbbc6453
Marcus D. Hanwell <mhanwell@bnl.gov>
2020-10-08 10:53:04 -0400

    The prefix is injected, not needed at call sites
    The axiosInstance injects the /qs prefix so that it can be changed at
    one site, a previous commit added the prefix in explicitly.

--- src/queueserver.ts
+++ src/queueserver.ts
@@ -78,7 +78,7 @@ export interface IPlanObjectsState {
 }
 
 export const getQueuedPlans = async(): Promise<IPlanObject[]> => {
-    const res = await axiosInstance.get('/qs/queue/get');
+    const res = await axiosInstance.get('/queue/get');
     console.log(res);
     return res.data.queue;
 }
@@ -89,7 +89,7 @@ export interface IPlanSubmitState {
 }
 
 export const submitPlan = async(): Promise<IPlanObject> => {
-    const res = await axiosInstance.post('/qs/queue/plan/add',
+    const res = await axiosInstance.post('/queue/plan/add',
         {
             plan:
             {
@@ -103,7 +103,7 @@ export const submitPlan = async(): Promise<IPlanObject> => {
 }
 
 export const clearQueue = async(): Promise<IPlan> => {
-    const res = await axiosInstance.post('/qs/queue/clear',
+    const res = await axiosInstance.post('/queue/clear',
         {});
     console.log(res);
     return res.data;
