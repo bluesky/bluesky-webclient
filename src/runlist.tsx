6f679ccc4c24367e72f47ffde5503b4e60d7dfd9
gbischof <bisc8233@gmail.com>
2020-10-20 11:35:02 -0400

    add delete button for list items

--- src/runlist.tsx
+++ src/runlist.tsx
@@ -4,18 +4,25 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import StarIcon from '@material-ui/icons/Star';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 export function RunList(props: any) {
   return (<List>
               {props.plans.map((planObject: any) => (
-                  <ListItem divider={true} key={planObject.plan_uid}>
+                  <ListItem divider={true} button={true} key={planObject.plan_uid}>
                       <ListItemIcon>
                         <StarIcon />
                       </ListItemIcon>
                       <ListItemText
                         primary={planObject.plan_uid.substr(0,8)}
-                        secondary={planObject.name}
-                      />
+                        secondary={planObject.name}/>
+                      <ListItemSecondaryAction>
+                        <IconButton edge="end" aria-label="comments">
+                          <DeleteForeverIcon />
+                        </IconButton>
+                      </ListItemSecondaryAction>
                   </ListItem>
               ))}
           </List>
