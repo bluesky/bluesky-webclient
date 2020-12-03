a32fd5b18ae7efbc19dd3bf9d85f2571302884a1
gbischof <bisc8233@gmail.com>
2020-12-03 13:28:49 -0500

    remove style

--- src/GenericPlanForm.tsx
+++ src/GenericPlanForm.tsx
@@ -25,23 +25,6 @@ interface IState {
   plan: ISumbitPlanObject;
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
-  gridList: {
-    width: 500,
-    height: 450,
-  },
-  icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
-  },
-}));
-
 export class GenericPlanForm extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
