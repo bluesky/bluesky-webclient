51c100a49e410ab664687fa5d816605139d4df82
gbischof <bisc8233@gmail.com>
2020-12-04 14:32:22 -0500

    change primary color

--- src/theme.tsx
+++ src/theme.tsx
@@ -5,7 +5,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#303030',
     },
     secondary: {
       main: '#19857b',
