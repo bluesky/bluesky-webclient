f0b98983efab3d839263c448f6c7524969215f4a
gbischof <bisc8233@gmail.com>
2020-12-23 15:22:49 -0500

    add regionChange callbaack

--- src/XAFSPlanForm.tsx
+++ src/XAFSPlanForm.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { IAllowedPlans, IParameter, ISumbitPlanObject } from './queueserver';
-import { Box, Button, FormControl, FormControlLabel, FormLabel, Grid, GridList, GridListTile, List, ListItem, ListItemText, Radio, RadioGroup, Select, Switch, TextField, Tooltip } from '@material-ui/core';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
+import { IAllowedPlans, ISumbitPlanObject } from './queueserver';
+import { Box, Button, FormControl, FormControlLabel, FormLabel, GridList, GridListTile, Radio, RadioGroup, Slider, TextField } from '@material-ui/core';
 
 const defaultBounds = [-200, -30, -10, 15.5, 15000];
 const defaultSteps = [10, 2, 0.3, 0.05];
 const defaultTimes = [0.5, 0.5, 0.5, 0.25];
+const defaultRegions = 4;
 
 type IProps = {
   name: string;
@@ -21,6 +19,7 @@ type IProps = {
 
 interface IState {
   plan: ISumbitPlanObject;
+  numRegions: number | number[];
 }
 
 export class XAFSPlanForm extends React.Component<IProps, IState> {
@@ -28,7 +27,8 @@ export class XAFSPlanForm extends React.Component<IProps, IState> {
     super(props);
     this.state = {
       plan: {name: this.props.name,
-             kwargs: {}}
+             kwargs: {}},
+      numRegions: defaultRegions
     }
   }
 
@@ -40,23 +40,11 @@ export class XAFSPlanForm extends React.Component<IProps, IState> {
         plan: new_plan
     });
   }
-
-  private addParameter(name: string){
-    const new_plan = this.state.plan;
-    new_plan.kwargs[name].push("");
+  
+  private changeRegions(event: object, value: number | number[]) {
     this.setState({
-        plan: new_plan
+        numRegions: value
     });
-  }
-
-  private removeParameter(name: string){
-    if (this.state.plan.kwargs[name].length > 1){
-      const new_plan = this.state.plan;
-      new_plan.kwargs[name].pop();
-      this.setState({
-          plan: new_plan
-      });
-    }
   }
 
   private submit(){
@@ -67,7 +55,6 @@ export class XAFSPlanForm extends React.Component<IProps, IState> {
     });
     this.props.submitPlan(this.state.plan)
   }
-
 
   static getDerivedStateFromProps(props : IProps, current_state: IState) {
     const temp_dict: Record<string, (string|number)[]> = {};
@@ -95,80 +82,82 @@ export class XAFSPlanForm extends React.Component<IProps, IState> {
             <CardContent>
               <div>
                 <GridList spacing={3} cellHeight="auto">
-                    <GridListTile key="Subheader" cols={2} style={{ color: "black", border:5, height: 'auto'}}>
-                      <Box borderBottom={3}>
-                        <Typography align="center" variant="h5" component="h1" >
-                          {this.props.name}
-                        </Typography>
-                        <Typography align="center" gutterBottom>
-                          {this.props.allowedPlans.plans_allowed[this.props.name]["description"] ?
-                            this.props.allowedPlans.plans_allowed[this.props.name]["description"] : "No plan description found."}
-                        </Typography>
-                      </Box>
-                    </GridListTile>
-                    <GridListTile style={{ height: 'auto' }}>
-                      <form noValidate autoComplete="off">
-                        <div>
-                            <TextField onChange={this.onChange.bind(this)} required 
-                                       name="element" id="0" label="Element" defaultValue="Au" /> &nbsp;
-                            <TextField onChange={this.onChange.bind(this)} required 
-                                       name="edge" id="0" label="Edge" defaultValue="K" />
-                        </div>
-                        <FormControl fullWidth>
-                            <TextField onChange={this.onChange.bind(this)} required 
-                                       name="sample" id="0" label="Sample" />
-                        </FormControl>
-                        <FormControl fullWidth>
-                            <TextField onChange={this.onChange.bind(this)} 
-                                       name="prop" id="0" label="Preparation" />
-                        </FormControl>
-                        <FormControl fullWidth>
-                            <TextField onChange={this.onChange.bind(this)} 
-                                       name="comment" id="0" label="Comment" multiline rows={4} />
-                        </FormControl>
-                        <div>
-                            <TextField onChange={this.onChange.bind(this)} required 
-                                       name="nscans" id="0" label="Number of scans" type="number" defaultValue="3" /> &nbsp;
-                            <TextField onChange={this.onChange.bind(this)} required 
-                                       name="start" id="0" label="Start" type="number" defaultValue="1" />
-                        </div><br />
-                        <FormControl>
-                            <FormLabel component="legend">Mode</FormLabel>
-                            <RadioGroup onChange={this.onChange.bind(this)} row 
-                                        name="mode" id="mode" aria-label="Mode" defaultValue="transmission">
-                                <FormControlLabel value="transmission" control={<Radio />} label="Transmission" />
-                                <FormControlLabel value="fluorescence" control={<Radio />} label="Fluorescence" />
-                                <FormControlLabel value="both" control={<Radio />} label="Both" />
-                            </RadioGroup>
-                        </FormControl><br />
-                        <FormControl>
-                            <FormLabel component="legend">Regions:</FormLabel>
-                        </FormControl><br />
-                        <div>
-                            <FormLabel component="legend">Bounds:</FormLabel>
-                            {defaultBounds.map((value: number, index) => (
-                                <TextField onChange={this.onChange.bind(this)} required 
-                                           name="bounds" id={String(index)} style={{width: 60}} defaultValue={value} />
-                            ))}
-                        </div>
-                        <div>
-                            <FormLabel component="legend">Steps:</FormLabel>
-                            {defaultSteps.map((value: number, index) => (
-                                <TextField onChange={this.onChange.bind(this)} required 
-                                           name="steps" id={String(index)} style={{width: 60}} defaultValue={value} />
-                            ))}
-                        </div>
-                        <div>
-                            <FormLabel component="legend">Times:</FormLabel>
-                            {defaultTimes.map((value: number, index) => (
-                                <TextField onChange={this.onChange.bind(this)} required 
-                                           name="times" id={String(index)} style={{width: 60}} defaultValue={value} />
-                            ))}
-                        </div><br />
-                        <Button variant="contained">Submit</Button>
-                      </form>
-                    </GridListTile>
-                  </GridList>     
+                  <GridListTile key="Subheader" cols={2} style={{ color: "black", border:5, height: 'auto'}}>
+                    <Box borderBottom={3}>
+                      <Typography align="center" variant="h5" component="h1" >
+                        {this.props.name}
+                      </Typography>
+                      <Typography align="center" gutterBottom>
+                        {this.props.allowedPlans.plans_allowed[this.props.name]["description"] ?
+                          this.props.allowedPlans.plans_allowed[this.props.name]["description"] : "No plan description found."}
+                      </Typography>
+                    </Box>
+                  </GridListTile>
+                  <GridListTile style={{ height: 'auto' }}>
+                    <form noValidate autoComplete="off">
+                      <div>
+                        <TextField onChange={this.onChange.bind(this)} required 
+                                    name="element" id="0" label="Element" defaultValue="Au" /> &nbsp;
+                        <TextField onChange={this.onChange.bind(this)} required 
+                                    name="edge" id="0" label="Edge" defaultValue="K" />
+                      </div>
+                      <FormControl fullWidth>
+                        <TextField onChange={this.onChange.bind(this)} required 
+                                    name="sample" id="0" label="Sample" />
+                      </FormControl>
+                      <FormControl fullWidth>
+                        <TextField onChange={this.onChange.bind(this)} 
+                                    name="prop" id="0" label="Preparation" />
+                      </FormControl>
+                      <FormControl fullWidth>
+                        <TextField onChange={this.onChange.bind(this)} 
+                                    name="comment" id="0" label="Comment" multiline rows={4} />
+                      </FormControl>
+                      <div>
+                        <TextField onChange={this.onChange.bind(this)} required 
+                                    name="nscans" id="0" label="Number of scans" type="number" defaultValue="3" /> &nbsp;
+                        <TextField onChange={this.onChange.bind(this)} required 
+                                    name="start" id="0" label="Start" type="number" defaultValue="1" />
+                      </div><br />
+                      <FormControl>
+                        <FormLabel component="legend">Mode</FormLabel>
+                        <RadioGroup onChange={this.onChange.bind(this)} row 
+                                    name="mode" id="mode" aria-label="Mode" defaultValue="transmission">
+                          <FormControlLabel value="transmission" control={<Radio />} label="Transmission" />
+                          <FormControlLabel value="fluorescence" control={<Radio />} label="Fluorescence" />
+                          <FormControlLabel value="both" control={<Radio />} label="Both" />
+                        </RadioGroup>
+                      </FormControl><br />
+                        <FormLabel component="legend">Regions:</FormLabel>
+                        <Slider onChange={this.changeRegions.bind(this)} 
+                          defaultValue={4} aria-labelledby="discrete-slider"
+                          valueLabelDisplay="auto" step={1} marks min={0} max={10}
+                          style={{height: "auto", width: '200'}}
+                        />
+                      <div>
+                          <FormLabel component="legend">Bounds:</FormLabel>
+                          {defaultBounds.map((value: number, index) => (
+                              <TextField onChange={this.onChange.bind(this)} required 
+                                          name="bounds" id={String(index)} style={{width: 60}} defaultValue={value} />
+                          ))}
+                      </div>
+                      <div>
+                          <FormLabel component="legend">Steps:</FormLabel>
+                          {defaultSteps.map((value: number, index) => (
+                              <TextField onChange={this.onChange.bind(this)} required 
+                                          name="steps" id={String(index)} style={{width: 60}} defaultValue={value} />
+                          ))}
+                      </div>
+                      <div>
+                          <FormLabel component="legend">Times:</FormLabel>
+                          {defaultTimes.map((value: number, index) => (
+                              <TextField onChange={this.onChange.bind(this)} required 
+                                          name="times" id={String(index)} style={{width: 60}} defaultValue={value} />
+                          ))}
+                      </div><br />
+                    </form>
+                  </GridListTile>
+                </GridList>     
               </div>
             </CardContent>
             <CardActions disableSpacing style={{ width: '100%', justifyContent: 'flex-end' }}>
