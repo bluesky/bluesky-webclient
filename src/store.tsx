12372d0aa59873513512d1f7c64295bc6e27b8d3
gbischof <bisc8233@gmail.com>
2022-02-11 14:11:14 -0500

    add redux-websocket

--- src/store.tsx
+++ src/store.tsx
@@ -30,6 +30,27 @@ const rootReducer = combineReducers<IApplicationState>({
     status: statusReducer,
 })
 
+const createMySocketMiddleware = (url: any) => {
+    return storeAPI => {
+        let socket = createMyWebsocket(url);
+
+        socket.on("message", (message: any) => {
+            storeAPI.dispatch({
+                type : "SOCKET_MESSAGE_RECEIVED",
+                payload : message
+            });
+        });
+        
+        return next => action => {
+            if(action.type == "SEND_WEBSOCKET_MESSAGE") {
+                socket.send(action.payload);
+                return;
+            }
+            return next(action);
+        }
+    }
+}
+
 export default function configureStore(): Store<IApplicationState> {
     const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     const store = createStore(rootReducer, undefined, composeEnhancers(applyMiddleware(thunk)));
